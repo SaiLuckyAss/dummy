693384e8a1496e8310d755c30924e76c82da9ace
SaiLuckyAss <62280154+SaiLuckyAss@users.noreply.github.com>
2023-06-25 21:27:14 +0530

    Create onboard.bash

---
+++ scripts/onboard.bash
@@ -0,0 +1,8 @@
+
+git config --global user.email "kulaisaikumar0906@gmail.com"
+git config --global username "SaiLuckyAss"
+git clone "https://{{secrets.GIT_TOKEN}}@github.com/SaiLuckyAss/hello-world.git
+echo "some text" > test.txt
+git add -all
+git commit -m "this is created by ctrl"
+git push -u origin master
